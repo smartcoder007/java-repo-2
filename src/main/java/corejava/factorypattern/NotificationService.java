398b8bee5c5db4b7dc903212229071d25e90aaae
kareem <kareemmca@gmail.com>
2024-03-05 10:13:09 +0530

    refactor

--- src/main/java/corejava/factorypattern/NotificationService.java
+++ src/main/java/corejava/factorypattern/NotificationService.java
@@ -1,8 +1,60 @@
 package corejava.factorypattern;
 
+
+interface Notification {
+    void notifyUser();
+}
+
+
+class PushNotification implements Notification {
+
+    @Override
+    public void notifyUser() {
+        // TODO Auto-generated method stub
+        System.out.println("Sending a push notification");
+    }
+}
+
+class SMSNotification implements Notification {
+
+    @Override
+    public void notifyUser() {
+        // TODO Auto-generated method stub
+        System.out.println("Sending an SMS notification");
+    }
+}
+
+class EmailNotification implements Notification {
+
+    @Override
+    public void notifyUser() {
+        // TODO Auto-generated method stub
+        System.out.println("Sending an e-mail notification");
+    }
+}
+
+
+class NotificationFactory {
+    public static Notification createNotification(String channel) {
+        if (channel == null || channel.isEmpty())
+            return null;
+        switch (channel) {
+            case "SMS":
+                return new SMSNotification();
+            case "EMAIL":
+                return new EmailNotification();
+            case "PUSH":
+                return new PushNotification();
+            default:
+                throw new IllegalArgumentException("Unknown channel " + channel);
+        }
+
+    }
+}
+
 public class NotificationService {
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Notification notification = NotificationFactory.createNotification("SMS");
         notification.notifyUser();
     }
